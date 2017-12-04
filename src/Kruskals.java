60f29d69b57077583a79df906832f7d94fba5187
Bodaciouslycrazy <bodiemalik@gmail.com>
2017-12-04 17:30:01 -0600

    added main program

---
+++ src/Kruskals.java
@@ -0,0 +1,17 @@
+//Bodie Malik
+//December 4, 2017
+//blerg
+
+//Step by step process:
+// 1 - read from ass9_data.csv
+// 2 - generate graph
+// 3 - apply kruskals algorithm
+
+public class Kruskals {
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
